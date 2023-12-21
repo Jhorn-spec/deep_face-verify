2da8178c36e4f9ad1486b434e49ea363988d9eb9
John <ajayijhorn@yahoo.com>
2023-12-21 14:04:46 +0100

    get image embedding

--- facemodel/ImageProcessor/image.py
+++ facemodel/ImageProcessor/image.py
@@ -3,3 +3,20 @@ def get_image_id(image):
 
 def process_image_id(image):
         pass
+
+def store_embedding(IMG_PATH):
+    data_embed = {}
+    for dirs in os.listdir(IMG_PATH):
+        if dirs not in data_embed.keys():
+            data_embed[dirs] = []
+        for obj in os.listdir(os.path.join(IMG_PATH, dirs)):
+            path = os.path.join(IMG_PATH, dirs, obj)
+            if path.endswith(".jpg"):
+                try:
+                    embed_arr = get_embedding(path)
+                    data_embed[dirs].append(embed_arr)
+                except ValueError:
+                    print("Face detection error in dir {}".format(dirs))
+                    
+
+    return data_embed
