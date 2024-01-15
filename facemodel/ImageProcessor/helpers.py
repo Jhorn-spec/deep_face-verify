f07b8cc3092476581e82481ba27e195496e840ce
John <ajayijhorn@yahoo.com>
2024-01-15 10:17:36 +0100

    add helper file

---
+++ facemodel/ImageProcessor/helpers.py
@@ -0,0 +1,85 @@
+import os
+import cv2
+from deepface import DeepFace
+import matplotlib.pyplot as plt
+
+backends = [
+  'opencv', 
+  'ssd', 
+  'dlib', 
+  'mtcnn', 
+  'retinaface', 
+  'mediapipe',
+  'yolov8',
+  'yunet',
+  'fastmtcnn',
+]
+
+df_metrics = ["cosine", "euclidean", "euclidean_l2"]
+
+models = [
+  "VGG-Face", 
+  "Facenet", 
+  "Facenet512", 
+  "OpenFace", 
+  "DeepFace", 
+  "DeepID", 
+  "ArcFace", 
+  "Dlib", 
+  "SFace",
+]
+
+def check_duplicate(id):
+    pass
+    # if id+'.jpg' in os.listdir(os.getcwd() + '/img_db/'):
+    #     return True
+    # else:
+    #     return False
+    
+
+def visulaize_frame(frame):
+    # Convert BGR to RGB (OpenCV uses BGR by default)
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Display the image using Matplotlib
+    plt.imshow(image_rgb)
+    plt.axis('off')  # Turn off axis labels
+    plt.show()
+
+
+def get_id(list_list_df):
+    df = list_list_df[0][0]
+    df['identity'] = df['identity'].apply(lambda x: x.split("/")[-1])
+    name = df.iloc[0][0]
+    return name
+
+
+def delete_representations():
+    for obj in os.listdir(os.getcwd() + '/img_db'):
+        if obj.endswith('.pkl'):
+            path = os.path.join(os.getcwd() + '/img_db/', obj)
+            os.remove(path)
+            print('previous representations deleted')
+    return
+
+
+def detector(frame, enforce=True):
+    try:
+        return DeepFace.extract_faces(frame, detector_backend=backends[0], enforce_detection=enforce)
+    except ValueError:
+        return None
+    
+
+def detection(frame):
+    try:
+        dfs = DeepFace.find(img_path=frame, db_path="../images/img_db/", 
+                            model_name="Facenet512", distance_metric="euclidean_l2")
+        print('face found')
+        return dfs
+    except ValueError:
+        print('face cannot be detected')
+        return None
+    
+
+
+    
--- facemodel/ImageProcessor/image.py
+++ facemodel/ImageProcessor/image.py
@@ -1,60 +1,99 @@
-import numpy as np
-import os 
-
-def get_image_id(image):
-        pass
-
-# If image database pre-exists
-# run this function, 
-# else... comment it out 
-def store_embedding(IMG_PATH):
-    data_embed = {}
-    for dirs in os.listdir(IMG_PATH):
-        if dirs not in data_embed.keys():
-            data_embed[dirs] = []
-        for obj in os.listdir(os.path.join(IMG_PATH, dirs)):
-            path = os.path.join(IMG_PATH, dirs, obj)
-            if path.endswith(".jpg"):
-                try:
-                    embed_arr = get_embedding(path)
-                    data_embed[dirs].append(embed_arr)
-                except ValueError:
-                    print("Face detection error in dir {}".format(dirs))
-                    
-
-    return data_embed
+from helpers import backends, detector, models, df_metrics, delete_representations, get_id
+import cv2
+from cv2 import VideoCapture
+from deepface import DeepFace
 
 
-def cosine_similarity(a, b):
-    dot_product = np.dot(a,b)
-    norm_a = np.linalg.norm(a)
-    norm_b = np.linalg.norm(b)
-    similarity = dot_product / (norm_a * norm_b)
 
-    return similarity
+def register_face(id):
+    # check for duplicate id
+    if id + '.jpg' in os.listdir(os.getcwd() + '/img_db/'):
+        print("id already exists")
+        check = input("Enter new id or enter 'x' to quit: ").lower()
+        if check == 'x':
+            print("exiting command...")
+            return
+        else:
+            id = check
+            
+    cap = VideoCapture(0)
 
-# Load the dictionary from the file using
-# embed_dict = np.load('embed_dict.npz', allow_pickle=True)
-def process_image_id(image_file, embed_dict):
-    best_match = {'match':0.25}
-    ID = None
+    while True:
+        ret, frame = cap.read()
+        face = detector(frame)
+        if face:
+            # crop and save image
+            x,y,w,h = face[0]['facial_area'].values()
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+            crop_img = frame[y:y+h, x:x+w]
+            cv2.imshow('detect face', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            elif cv2.waitKey(1) & 0xFF == ord('c'):
+                # save face
+                face_path = "/img_db/"+id+".jpg"
+                cv2.imwrite(face_path, crop_img)
+                print(f" id {id} registered successfully")
+                print(f"image stored in {face_path}")
+                delete_representations()
+                
+                break
+        else:
+            # if no face detected
+            h,w = frame.shape[:2]
+            text = 'Adjust face and brightness'
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontsize = 1  
+            color = (0,255,0)
+            thickness = 2
+            text_size, _ = cv2.getTextSize(text, font, fontsize, thickness)
 
-    # get embedding from img_file
-    img_embed_arr = get_embedding(image_file)
+            x = (w - text_size[0]) // 2  
+            y = (h + text_size[1]) // 2
+            cv2.putText(frame, text, (x, y), font, fontsize, color, thickness)
+            # cv2.putText(frame, text, (10, h - 20), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0,), 1)
+            # cv2.putText(crop_img, 'Align face', (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+            cv2.imshow('detect face', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-    for id, embeddings in embed_dict.items():
-        if embeddings:
-            for embedding in embeddings:
-                similarity_check = cosine_similarity(img_embed_arr, embedding)
-                if similarity_check < best_match['match']:
-                    best_match[id] = id
-                    best_match['embedding'] = embedding
-                    best_match['match'] = similarity_check
-                    ID = id
-                    print("Match found")
+    cap.release()
+    cv2.destroyAllWindows() 
 
-    if ID is not None:
-        return ID
-    else:
-        return "User not in database"
-    # plot the user embedding
+
+
+def face_detect():
+    cap = cv2.VideoCapture(0)
+    dfs = []
+    
+    while True:
+        ret, frame = cap.read()
+
+        if ret == False:
+            break
+        
+        face = detector(frame)
+        if face is not None:
+            # face detection and recognition logic
+            x,y,w,h = face[0]['facial_area'].values()  
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255, 0), 3)
+            # cv2.imshow('detect face', frame)
+            
+            if cv2.waitKey(1) & 0xFF == ord('c'):
+                df = DeepFace.find(img_path=frame, db_path="./img_db/", model_name=models[2], 
+                                   distance_metric=df_metrics[2], enforce_detection=False)
+                                #    detector_backend=backends[0])
+                dfs.append(df)
+                break
+
+        cv2.imshow('display', frame)
+                
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+            
+    cap.release()
+    cv2.destroyAllWindows()
+
+    user_id = get_id(dfs)
+    
+    return user_id
