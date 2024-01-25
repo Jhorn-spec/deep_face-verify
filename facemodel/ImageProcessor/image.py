13e2791414550b92c5053b95adfb1d770d3b5a7a
John <ajayijhorn@yahoo.com>
2024-01-25 14:35:23 +0100

    update changes

--- facemodel/ImageProcessor/image.py
+++ facemodel/ImageProcessor/image.py
@@ -5,95 +5,165 @@ from deepface import DeepFace
 
 
 
-def register_face(id):
+def register_face(id, image_path=None, live = False):
+    result = {"success": False, "message": "", "id": id, "image_path": ""}
+
     # check for duplicate id
     if id + '.jpg' in os.listdir(os.getcwd() + '/img_db/'):
         print("id already exists")
+        result["message"] = "id already exists"
         check = input("Enter new id or enter 'x' to quit: ").lower()
         if check == 'x':
             print("exiting command...")
-            return
+            result["message"] = "exiting command..."
+            return result
         else:
             id = check
             
-    cap = VideoCapture(0)
+    if live:
+        # take in image from cam
+        cap = VideoCapture(0)
+        
+        while True:
+            ret, frame = cap.read()
+            face = detector(frame)
+            if face:
+                # crop and save image
+                x, y , w, h = face[0]['facial_area'].values()
+                cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+                crop_img = frame[y:y+h, x:x+w]
 
-    while True:
-        ret, frame = cap.read()
+                # Display the image with the detected face
+                cv2.imshow('detect face', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    result["message"] = "quit detected. No face detected in the provided image"
+                    break
+                    
+                if cv2.waitKey(1) & 0xFF == ord('c'):
+                    # Save face
+                    face_path = os.path.join(os.getcwd() + '/img_db/', id + '.jpg')
+                    # face_path = "/img_db/"+id+".jpg"
+                    cv2.imwrite(face_path, crop_img)
+                    print(f" id {id} registered successfully")
+                    print(f"image stored in {face_path}")
+
+                    delete_representations()
+                    result["success"] = True
+                    result["message"] = f"id {id} registered successfully"
+                    result["image_path"] = face_path
+                    break
+            
+            else:
+                # if no face detected
+                h,w = frame.shape[:2]
+                text = 'Adjust face and brightness'
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontsize = 1  
+                color = (0,255,0)
+                thickness = 2
+                text_size, _ = cv2.getTextSize(text, font, fontsize, thickness)
+
+                x = (w - text_size[0]) // 2  
+                y = (h + text_size[1]) // 2
+                cv2.putText(frame, text, (x, y), font, fontsize, color, thickness)
+                # cv2.putText(frame, text, (10, h - 20), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0,), 1)
+                # cv2.putText(crop_img, 'Align face', (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+                cv2.imshow('detect face', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    result["message"] = "quit detected. No face detected in the provided image"
+                    break
+                
+        cv2.destroyAllWindows()
+        cap.release()
+        return result
+                    
+
+    # Load the image
+    else:
+        # check image_path is not None
+        if image_path is None:
+            result["message"] = "image_path is None"
+            print(result["message"])
+            return result
+        
+        frame = cv2.imread(image_path)
+
+        # perform face detection
         face = detector(frame)
         if face:
             # crop and save image
-            x,y,w,h = face[0]['facial_area'].values()
+            x, y , w, h = face[0]['facial_area'].values()
             cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
             crop_img = frame[y:y+h, x:x+w]
+
+            # Display the image with the detected face
             cv2.imshow('detect face', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-            elif cv2.waitKey(1) & 0xFF == ord('c'):
-                # save face
-                face_path = "/img_db/"+id+".jpg"
-                cv2.imwrite(face_path, crop_img)
-                print(f" id {id} registered successfully")
-                print(f"image stored in {face_path}")
-                delete_representations()
-                
-                break
+            # cv2.waitKey(0)
+
+            # Save face
+            face_path = os.path.join(os.getcwd() + '/img_db/', id + '.jpg')
+            # face_path = "/img_db/"+id+".jpg"
+            cv2.imwrite(face_path, crop_img)
+            print(f" id {id} registered successfully")
+            print(f"image stored in {face_path}")
+
+            delete_representations()
+            result["success"] = True
+            result["message"] = f"id {id} registered successfully"
+            result["image_path"] = face_path
+            return result
+        
         else:
-            # if no face detected
-            h,w = frame.shape[:2]
-            text = 'Adjust face and brightness'
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            fontsize = 1  
-            color = (0,255,0)
-            thickness = 2
-            text_size, _ = cv2.getTextSize(text, font, fontsize, thickness)
-
-            x = (w - text_size[0]) // 2  
-            y = (h + text_size[1]) // 2
-            cv2.putText(frame, text, (x, y), font, fontsize, color, thickness)
-            # cv2.putText(frame, text, (10, h - 20), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0,), 1)
-            # cv2.putText(crop_img, 'Align face', (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
-            cv2.imshow('detect face', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-    cap.release()
-    cv2.destroyAllWindows() 
+            result["message"] = "No face detected in the provided image"
+            print(result["message"])
+            cv2.destroyAllWindows()
+            return result
 
 
-
-def face_detect():
+# take in the path to the image database
+def face_detect(db_path):
     cap = cv2.VideoCapture(0)
     dfs = []
-    
+
     while True:
         ret, frame = cap.read()
 
         if ret == False:
             break
-        
+
         face = detector(frame)
         if face is not None:
             # face detection and recognition logic
             x,y,w,h = face[0]['facial_area'].values()  
             cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255, 0), 3)
             # cv2.imshow('detect face', frame)
-            
+
             if cv2.waitKey(1) & 0xFF == ord('c'):
-                df = DeepFace.find(img_path=frame, db_path="./img_db/", model_name=models[2], 
-                                   distance_metric=df_metrics[2], enforce_detection=False)
+                df = DeepFace.find(img_path=frame, db_path=db_path, model_name=models[2], 
+                                distance_metric=df_metrics[2], enforce_detection=False)
                                 #    detector_backend=backends[0])
                 dfs.append(df)
                 break
 
         cv2.imshow('display', frame)
-                
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-            
+
     cap.release()
     cv2.destroyAllWindows()
 
     user_id = get_id(dfs)
-    
+
     return user_id
+
+
+'''
+use case of register_face function
+you can either upload an image or take in live image from camera
+
+register_face(id, image_path='path\to\image')
+or
+by setting live=True to use webcam
+register_face(id, live=True)
+'''
