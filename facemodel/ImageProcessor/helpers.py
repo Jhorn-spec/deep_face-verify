4deb4a8ba4ef2dece5a3a1c42f3137fe427d2fe8
John <ajayijhorn@yahoo.com>
2024-01-25 18:35:06 +0100

    add live function; update function

--- facemodel/ImageProcessor/helpers.py
+++ facemodel/ImageProcessor/helpers.py
@@ -1,7 +1,10 @@
+
 import os
 import cv2
+from cv2 import VideoCapture
 from deepface import DeepFace
 import matplotlib.pyplot as plt
+import uuid
 
 backends = [
   'opencv', 
@@ -28,6 +31,64 @@ models = [
   "Dlib", 
   "SFace",
 ]
+
+def live_capture():
+    cap = VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if ret == False:
+            break
+
+        face = detector(frame)
+        if face:
+            x,y,w,h = face[0]['facial_area'].values()
+            crop_img = frame[y:y+h, x:x+w]
+            cv2.imshow('display', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('c'):
+                face_path = os.path.join(os.getcwd(), '.temp')
+                if not os.path.exists(face_path):
+                    os.makedirs(face_path)
+
+                for obj in os.listdir(face_path):
+                    try:
+                        obj_path = os.path.join(face_path, obj)
+                        os.remove(obj_path)
+                    except Exception as e:
+                        print(e)
+                        pass
+                temp_file = os.path.join(face_path, '{}.jpg'.format(uuid.uuid1()))
+                cv2.imwrite(temp_file, crop_img)
+                break
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        else:
+            # if no face detected
+            h,w = frame.shape[:2]
+            text = 'Adjust face and brightness'
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontsize = 1  
+            color = (0,255,0)
+            thickness = 2
+            text_size, _ = cv2.getTextSize(text, font, fontsize, thickness)
+
+            x = (w - text_size[0]) // 2  
+            y = (h + text_size[1]) // 2
+            cv2.putText(frame, text, (x, y), font, fontsize, color, thickness)
+            # cv2.putText(frame, text, (10, h - 20), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0,), 1)
+            # cv2.putText(crop_img, 'Align face', (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+            cv2.imshow('display', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            
+
+        
+    cap.release()
+    cv2.destroyAllWindows()
+    return temp_file
+
 
 def check_duplicate(id):
     pass
@@ -67,7 +128,7 @@ def detector(frame, enforce=True):
     try:
         return DeepFace.extract_faces(frame, detector_backend=backends[0], enforce_detection=enforce)
     except Exception as e:
-        print(f"Image detection failed with error: {e}")
+        # print(f"Image detection failed with error: {e}")
         return None
     
 
@@ -80,7 +141,8 @@ def detection(frame):
     except ValueError:
         print('face cannot be detected')
         return None
-    
+
+
 
 
     
